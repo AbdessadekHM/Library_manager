64933247c44607df6113796a959ff61865449af6
AbdessadekHm <abdessadekhm@gmail.com>
2025-03-21 16:07:56 +0000

    Add Get documents service

--- src/main/java/com/main/library_management/services/DocumentService.java
+++ src/main/java/com/main/library_management/services/DocumentService.java
@@ -3,6 +3,9 @@ package com.main.library_management.services;
 
 import java.io.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.main.library_management.dao.impl.DocumentDAO;
+import com.main.library_management.models.Document;
 import jakarta.servlet.Servlet;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -14,25 +17,40 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+
+
+@WebServlet(name = "document", value = "/document")
 public class DocumentService extends HttpServlet {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+
+
+
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
 
-        // Hello
+        List<Document> documents = DocumentDAO.getInstance().getAllDocuments();
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        // Create a sample response object
+
+
+        // Convert object to JSON string
+        String jsonResponse = objectMapper.writeValueAsString(documents);
+
+        // Send JSON response
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        out.print(jsonResponse);
+        out.flush();
+
+
+
     }
 
     public void destroy() {
